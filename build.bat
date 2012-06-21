6b5f23baaa17c460fff6d4025c6ea2fc7feeb2f7
Gabor Liptak <liptakgabor@freemail.hu>
2012-06-21 14:44:31 +0200

    Run time optimization

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ path=C:\DEVENV\CYGWIN\V1_7_9\bin;C:\Programs\Sourcery G++ Lite\bin;%PATH%
 rem set TMPDIR=c:\tmp
 set WORK_ROOT=%CD%
 
-bash --login -i -c "make -C $WORK_ROOT %1 %2 %3 %4"
+bash -i -c "make -C $WORK_ROOT %1 %2 %3 %4"
