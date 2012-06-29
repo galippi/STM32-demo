47b94e1043f50f59c88822a7060722699ab652aa
Gabor Liptak <liptakgabor@freemail.hu>
2012-06-29 15:54:58 +0200

    SysTick handler is implemented

--- blinker02.c
+++ blinker02.c
@@ -1,4 +1,6 @@
-#include "stdint.h"
+#include <stdint.h>
+#include <string.h>
+
 #include "stm32f0xx.h"
 #include "stm32f0xx_gpio.h"
 
@@ -10,18 +12,10 @@ struct s
   char v3;
 }s_var;
 
-double fv = 5.0;
-
 GPIO_TypeDef * const gpioa = GPIOA;
 GPIO_TypeDef * const gpioc = GPIOC;
 RCC_TypeDef * const rcc = RCC;
-
-static void func(void)
-{
-  s_var.v1 += 5;
-  //fv = fv - 2.0;
-  fv = - 2.0;
-}
+SysTick_Type * const systick = SysTick;
 
 #define BitfieldSet(var, bit_num, bit_length, val) ((var) = ((var) & (~(((1 << (bit_length)) - 1)) << (bit_num)) | ((val) << (bit_num))))
 
@@ -87,7 +81,7 @@ inline void Button1_Init(void)
   GPIO_PortInit_In(BUTTON1_PORT, BUTTON1_PIN_NUM);
 }
 
-uint8_t Button1_Get(void)
+inline uint8_t Button1_Get(void)
 {
   return (BUTTON1_PORT->IDR & (1 << BUTTON1_PIN_NUM)) ? 1 : 0;
 }
@@ -97,7 +91,7 @@ inline void LED3_Init(void)
   GPIO_PortInit_Out(LED3_PORT, LED3_PIN_NUM);
 }
 
-void LED3_Set(uint8_t val)
+inline void LED3_Set(uint8_t val)
 {
   if (val)
   {
@@ -113,7 +107,7 @@ inline void LED4_Init(void)
   GPIO_PortInit_Out(LED4_PORT, LED4_PIN_NUM);
 }
 
-void LED4_Set(uint8_t val)
+inline void LED4_Set(uint8_t val)
 {
   if (val)
   {
@@ -124,27 +118,72 @@ void LED4_Set(uint8_t val)
   }
 }
 
+void SysTick_Init(void)
+{
+  uint32_t ticks = 65536;
+  SysTick->LOAD  = (ticks & SysTick_LOAD_RELOAD_Msk) - 1;      /* set reload register */
+  //NVIC_SetPriority (SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);  /* set Priority for Cortex-M0 System Interrupts */
+  SysTick->VAL   = 0;                                          /* Load the SysTick Counter Value */
+  SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
+                   SysTick_CTRL_TICKINT_Msk   |
+                   SysTick_CTRL_ENABLE_Msk;                    /* Enable SysTick IRQ and SysTick Timer */
+}
+
+uint8_t timer[128];
+#define SIZE_TIMER (sizeof(timer)/sizeof(timer[0]))
+#define MAX_TIMER 255
+uint8_t timer_brake;
+
+inline uint16_t SysTick_Get(void)
+{
+  uint16_t val = SysTick->VAL & 0xFFFF;
+  if (!timer_brake)
+  {
+    uint16_t idx = val + (SIZE_TIMER / 2);
+    if (idx < SIZE_TIMER)
+    {
+      timer[idx] += 1;
+      if (timer[idx] == MAX_TIMER)
+      {
+        timer_brake = 1;
+      }
+    }
+  }
+  return val;
+}
+
 void main(void)
 {
   uint8_t led3 = 0;
+  SysTick_Init();
   LED3_Init();
   LED4_Init();
   Button1_Init();
+  //memset(timer, 0, sizeof(timer));
+  timer_brake = 0;
 
-  var1++;
-  s_var.v2 = s_var.v3 +22;
   s_var.v1 = 0;
-  while(s_var.v1 < 40)
+  //while(s_var.v1 < 40)
   {
-    func();
     {
       LED3_Set(led3);
       led3 = !led3;
       LED4_Set(led3);
       if (Button1_Get())
       {
-        var1++;
+        s_var.v1++;
       }
+    }
+  }
+  {
+    while (1)
+    {
+      LED3_Set(1);
+      while (SysTick_Get() > 32767)
+        ;
+      LED3_Set(0);
+      while (SysTick_Get() <= 32767)
+        ;
     }
   }
 }
