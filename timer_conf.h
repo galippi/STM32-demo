19980a076eb46a3d6b1929cbfe2bca3f664c9acc
Gabor Liptak <liptakgabor@freemail.hu>
2012-09-27 17:16:53 +0200

    The time-base config is corrected - prescaler to 7 (7+1=>8).

--- timer_conf.h
+++ timer_conf.h
@@ -3,7 +3,7 @@
 
 #define TIM3_CR1_INIT 0x10 /* down counter mode */
 #define TIM3_CR2_INIT 0x0
-#define TIM3_PSC_INIT  8 /* 8MHz / 8 -> 1MHz */
+#define TIM3_PSC_INIT  7 /* 8MHz / (7+1) -> 1MHz */
 #define TIM3_ARR_INIT 1000 /* 1MHz / 1000 -> 1ms timebase */
 #define TIM3_CCER_INIT 0x01 /* enable CC1 out high */
 #define TIM3_CCMR1_INIT 0x0000
