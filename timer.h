ef2cca1a229c4f353d1c067faed6a0804ec6d96a
Gabor Liptak <liptakgabor@freemail.hu>
2012-09-28 15:21:23 +0200

    TIM3-CC1 handling is implemented

--- timer.h
+++ timer.h
@@ -23,4 +23,29 @@ static inline void TIM3_SR_UIF_Reset(void)
   TIM3->SR = ~(TIM_SR_UIF); /* reset the UpdateInterrupFlag */
 }
 
+static inline uint16_t TIM3_Cnt_Get(void)
+{
+  return TIM3->CNT;
+}
+
+static inline uint32_t TIM3_SR_CC1IF_Get(void)
+{
+  return (TIM3->SR & TIM_SR_CC1IF);
+}
+
+static inline void TIM3_SR_CC1IF_Reset(void)
+{
+  TIM3->SR = ~(TIM_SR_CC1IF); /* reset the CC1-InterrupFlag */
+}
+
+static inline uint16_t TIM3_CCR1_Get(void)
+{
+  return TIM3->CCR1;
+}
+
+static inline void TIM3_CCR1_Set(uint16_t new_val)
+{
+  TIM3->CCR1 = new_val;
+}
+
 #endif /* _TIMER_H_ */
