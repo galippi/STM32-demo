052acf2a116bb822662be8d1a20e7ab14f1124ab
Gabor Liptak <liptakgabor@freemail.hu>
2012-10-08 18:37:12 +0200

    DMA turn on is corrected.

--- dma.h
+++ dma.h
@@ -11,13 +11,14 @@
  *  @{
  ******************************************************************************/
 
-
 #ifndef _DMA_H_
 #define _DMA_H_
 
+#include "stm32f0xx_rcc.h"
+
 static inline void DMA_Init(void)
 {
-  RCC->RCC_AHBENR |= RCC_AHBENR_DMA1EN; /* enable the DMA */
+  RCC->AHBENR |= RCC_AHBENR_DMA1EN; /* enable the DMA */
 }
 
 #endif /* _DMA_H_ */
