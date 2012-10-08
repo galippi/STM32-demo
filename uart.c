d6d65d55d4ce9ff02f76cb002f89a4c3d0257b6a
Gabor Liptak <liptakgabor@freemail.hu>
2012-10-08 18:38:08 +0200

    DMA is turned on for UART2-TX

--- uart.c
+++ uart.c
@@ -1,5 +1,6 @@
 #include "system_conf.h"
 #include "gpio.h"
+#include "dma.h"
 
 #include "uart.h"
 
@@ -20,8 +21,9 @@ void UART2_Init(void)
 #endif
   USART2->CR2 = 0; /* 1 stop bit */
   USART2->CR3 = 0x80; /* DMA is enabled for the channel */
-  USART2->CR1 |= USART_CR1_UE; /* USART2 is enabled */
   /* configuring DMA for USART2-TX */
+  DMA_Init();
   DMA1_Channel4->CPAR = &(USART2->TDR);
   DMA1_Channel4->CCR = DMA_CCR_DIR | DMA_CCR_MINC; /* mem2per, no-circ, no-per-inc, mem-inc, psize=8, memsize=8,ch-prio=low, no-mem2mem */
+  USART2->CR1 |= USART_CR1_UE; /* USART2 is enabled */
 }
