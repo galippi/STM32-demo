135aad3558eb0434cec34ef806d2d6de5ad891af
Gabor Liptak <liptakgabor@freemail.hu>
2012-10-09 15:18:00 +0200

    make debug options are added

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ path=C:\DEVENV\CYGWIN\V1_7_9\bin;C:\Programs\Sourcery G++ Lite\bin;%PATH%
 rem set TMPDIR=c:\tmp
 set WORK_ROOT=%CD%
 
-bash -i -c "make -C $WORK_ROOT %1 %2 %3 %4"
+::bash -i -c "make -d -r -C $WORK_ROOT %1 %2 %3 %4"
+bash -i -c "make -r -C $WORK_ROOT %1 %2 %3 %4"
