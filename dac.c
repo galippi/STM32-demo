194ea5261f926287e2e93b6ff361e1eb5cc0c4d2
Gabor Liptak <liptakgabor@freemail.hu>
2016-12-31 21:01:37 +0100

    Comments are corrected: DAC

--- dac.c
+++ dac.c
@@ -4,12 +4,12 @@
 
 void DAC_Init(void)
 {
-  /* enable the GPIO-A, if it was not enabled */
+  /* enable the DAC, if it was not enabled */
   if (!(RCC->APB1ENR & RCC_APB1ENR_DACEN))
   {
     RCC->APB1ENR |= RCC_APB1ENR_DACEN;
   }
-  /* enable the GPIO-A, if it was not enabled - DAC is connected to PA.4-5 */
+  /* Configure GPIOA.4 to analog - DAC is connected to PA.4-5 */
   GPIO_PortInit_Analog(GPIOA, 4);
   DAC->CR = (DAC->CR & 0x303F) | 0x0001;
 }
