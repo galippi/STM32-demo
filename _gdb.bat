fdcd4b921dc026aed7c552498e61c9dcd4750650
Gabor Liptak <liptakgabor@freemail.hu>
2017-01-08 12:12:46 +0100

    gdb starter script is added

--- _gdb.bat
+++ _gdb.bat
@@ -1,1 +1,2 @@
-start "%CD% gdb" "C:\Programs\Sourcery G++ Lite\bin\arm-none-eabi-gdb.exe" -se blinker02.gcc.thumb.elf
+@set HOME=D:\Gabor\ARM-STM32\STM32F103C8T6\
+start "%CD% gdb" C:\Programok\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-gdb.exe -se=./bin/demo1.elf -x=.targetinit
