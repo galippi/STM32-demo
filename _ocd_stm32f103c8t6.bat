1047158c846398ef3d452d3e8d14969d03543f3a
Gabor Liptak <liptakgabor@freemail.hu>
2017-01-08 12:07:53 +0100

    SWD starter script is added

---
+++ _ocd_stm32f103c8t6.bat
@@ -0,0 +1,3 @@
+@set OCD_PATH=C:\Programok\openocd-0.9.0
+::start %OCD_PATH%\bin-x64\openocd.exe -f scripts\board\st_nucleo_f103rb.cfg
+start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103rb.cfg
