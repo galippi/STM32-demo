9b89d953d2b45fd652a37b46183f15a442b6789f
Gabor Liptak <liptakgabor@freemail.hu>
2017-01-21 19:38:51 +0100

    Comment is added

--- timer.c
+++ timer.c
@@ -32,6 +32,6 @@ void TIM3_Init(void)
   TIM3->CCMR1 = TIM3_CCMR1_INIT;
   TIM3->CCMR2 = TIM3_CCMR2_INIT;
   TIM3->SR = 0x0000; /* clear all IT requests */
-  TIM3->DIER = (TIM3->DIER & 0xA0A0) | TIM3_DIER_INIT;
+  TIM3->DIER = (TIM3->DIER & 0xA0A0) | TIM3_DIER_INIT; /* enable update interrupt */
   TIM3->CR1 |= 1; /* enable timer */
 }
