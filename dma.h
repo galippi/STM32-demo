98d8bfb4f8e4c1cd20a22ec4f31d6af5472ea9d6
Gabor Liptak <liptakgabor@freemail.hu>
2018-03-15 10:32:18 +0100

    DMA init change to support more DMA modules

--- dma.h
+++ dma.h
@@ -17,10 +17,16 @@
 #include "controller.h"
 #include STM32_RCC_HEADER
 
-static inline void DMA_Init(void)
+static inline void DMA_Init(DMA_TypeDef *dma)
 {
 #if (CPU_TYPE == CPU_TYPE_STM32F0) || (CPU_TYPE == CPU_TYPE_STM32F1)
-  RCC->AHBENR |= RCC_AHBENR_DMA1EN; /* enable the DMA */
+  if (dma == DMA1)
+  {
+    RCC->AHBENR |= RCC_AHBENR_DMA1EN; /* enable the DMA */
+  }else
+  {
+    //RCC->AHBENR |= RCC_AHBENR_DMA2EN; /* enable the DMA */
+  }
 #elif CPU_TYPE == CPU_TYPE_STM32F4
   RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
 #else
