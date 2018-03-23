36bbe618cec9dc3da1d3c609f8e4a22e64c608b9
Liptak Gabor <gabor.liptak@knorr-bremse.com>
2018-03-23 16:55:01 +0100

    Clock source name is corrected to PLL

--- system_conf.h
+++ system_conf.h
@@ -3,9 +3,9 @@
 
 #define CLK_SOURCE_HSI      0
 #define CLK_SOURCE_HSE_OSC  1
-#define CLK_SOURCE_HSE_EXT  2
+#define CLK_SOURCE_HSE_PLL  2
 
-#define CLK_SOURCE CLK_SOURCE_HSE_EXT
+#define CLK_SOURCE CLK_SOURCE_HSE_PLL
 
 #define F_SYSTEM 48000 /* kHz */
 #define F_OSC     8000 /* kHz */
