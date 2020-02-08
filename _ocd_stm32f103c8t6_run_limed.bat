ecb7fcedaaa92f710277a20c27bca93414c788b8
Gabor Liptak <liptakgabor@freemail.hu>
2020-02-08 18:49:47 +0100

    limed scripts are updated

--- _ocd_stm32f103c8t6_run_limed.bat
+++ _ocd_stm32f103c8t6_run_limed.bat
@@ -2,5 +2,5 @@
 ::start %OCD_PATH%\bin-x64\openocd.exe -f scripts\board\st_nucleo_f103rb.cfg
 ::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103rb.cfg
 ::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103c8.cfg
-%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg -c "program ./bin/demo1.s19" -c "reset run" -c "exit"
+%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg -c "reset run" -c "exit"
 ::%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg
