65e07cb99c3dce7905e300c7c70436d0ba2f4d09
Gabor Liptak <liptakgabor@freemail.hu>
2020-02-08 18:24:54 +0100

    CPU-load measurement preparation

--- scheduler_preemptive.h
+++ scheduler_preemptive.h
@@ -1,6 +1,8 @@
 #ifndef _SCHEDULER_PREEMPTIVE_H_
 #define _SCHEDULER_PREEMPTIVE_H_
 
+extern uint8_t CPU_load;
+
 void SchedulerPre_Init(void);
 void SchedulerPre_TaskTableUpdate(void);
 void SchedulerPre_TaskManagement(void);
