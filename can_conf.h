f0a0f7235fae627ecfabf32240b7123e1f164327
Gabor Liptak <liptakgabor@freemail.hu>
2020-04-05 10:52:17 +0200

    CAN-init is simplified CAN-Rx, CAN-Tx and CAN-filter handling are implemented

--- can_conf.h
+++ can_conf.h
@@ -1,6 +1,12 @@
+#ifndef _CAN_CONF_H_
+#define _CAN_CONF_H_ 1
+
 #define CAN_TSEG1  8
 #define CAN_TSEG2  3
 #define CAN_SJW    1
-#define CAN_BTR_INIT 0
+//#define CAN_BTR_INIT (CAN_BTR_SILM | CAN_BTR_LBKM) // Loopback, Silent Mode (self-test)
+#define CAN_BTR_INIT 0 // normal mode
 
 #define CAN_MCR_INIT CAN_MCR_ABOM
+
+#endif /* _CAN_CONF_H_ */
