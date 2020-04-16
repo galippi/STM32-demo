8c2be8365ad0dcc92f37e92af9b908585eebd6e0
Gabor Liptak <liptakgabor@freemail.hu>
2020-04-16 06:59:10 +0200

    bitfield_lib: BitSet and BitReset are added

--- bitfield_lib.h
+++ bitfield_lib.h
@@ -2,5 +2,7 @@
 #define _BITFIELD_LIB_H_
 
 #define BitfieldSet(var, bit_num, bit_length, val) ((var) = (((var) & (~(((1 << (bit_length)) - 1) << (bit_num)))) | ((val) << (bit_num))))
+#define BitReset(var, bit_num) ((var) = (var) & ~(1 << (bit_num)))
+#define BitSet(var, bit_num)   ((var) = (var) |  (1 << (bit_num)))
 
 #endif /* _BITFIELD_LIB_H_ */
