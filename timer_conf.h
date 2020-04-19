9670b0de63f4304cc81de2423b0f5ebcbd6871a2
Gabor Liptak <liptakgabor@freemail.hu>
2020-04-19 10:46:11 +0200

    The modules are adapted for SysClock module

--- timer_conf.h
+++ timer_conf.h
@@ -1,7 +1,7 @@
 #ifndef _TIMER_CONF_H_
 #define _TIMER_CONF_H_
 
-#include "system_conf.h"
+#include "SysClock_conf.h"
 #include "scheduler_preemptive.h"
 
 static inline void TIM2_CC3IF_Callback(void)
@@ -11,10 +11,10 @@ static inline void TIM2_CC3IF_Callback(void)
 
 #define TIM3_CR1_INIT 0x00 /* up counter mode */
 #define TIM3_CR2_INIT 0x0
-#define TIM3_FREQ 1000 /* kHz */
-#define TIM3_PSC_INIT  ((F_SYSTEM / TIM3_FREQ) - 1) /* prescaler of TIM3 -> 1MHz */
-#define TIM3_ARR_INIT 1000 /* 1MHz / 1000 -> 1ms timebase */
-#define TIM3_CCER_INIT 0x01 /* enable CC1 out high */
+#define TIM3_FREQ 1000 /* Hz */
+#define TIM3_PSC_INIT  ((f_TIMXCLK_Hz / (TIM3_FREQ * 1000)) - 1) /* prescaler of TIM3 -> 1MHz */
+#define TIM3_ARR_INIT 65535 /* continuous counter */
+#define TIM3_CCER_INIT 0x0101 /* enable CC1 out high, CC3/CC4 input */
 #define TIM3_CCMR1_INIT 0x0000
 #define TIM3_CCMR2_INIT 0x0000
 #define TIM3_DIER_INIT 0x0001 /* enable update interrupt */
--- uart.c
+++ uart.c
@@ -2,17 +2,55 @@
 #include "gpio.h"
 #include "dma.h"
 #include "bitfield_lib.h"
+#include "SysClock_conf.h"
 
 #include "uart.h"
 
+void UART1_Init(uint32_t baudRate, uint8_t uartRemap)
+{
+  if (uartRemap == 0)
+  {
+    GPIO_PortInit_AFOut(GPIOA,  9); /* PA2 USART1_TX */
+    GPIO_PortInit_In(GPIOA, 10); /* PA3 USART2_RX */
+    BitfieldSet(AFIO->MAPR, 2, 1, 0); /* AFIO_MAPR_USART1_REMAP */
+  }else
+  {
+    GPIO_PortInit_AFOut(GPIOB,  6); /* PA2 USART1_TX */
+    GPIO_PortInit_In(GPIOB,  7); /* PA3 USART2_RX */
+    BitfieldSet(AFIO->MAPR, 2, 1, 1); /* AFIO_MAPR_USART1_REMAP */
+  }
+  RCC->APB2ENR |= RCC_APB2ENR_USART1EN;   /* enable the USART1 */
+  USART1->CR1 = USART_CR1_TE | USART_CR1_RE; /* TX/RX are enabled */
+  USART1->BRR = ((((f_APB2_Hz) / 1) + (baudRate / 2)) / baudRate);
+  USART1->CR2 = 0; /* 1 stop bit */
+#if UART1_DMA == 0
+  USART1->CR3 = 0x00; /* DMA is disabled (???) for the channel */
+#else
+  USART1->CR3 = 0xC0; /* DMA is enabled for the channel */
+  /* configuring DMA for USART1-TX */
+  DMA_Init(DMA1);
+  DMA1_Channel4->CCR &= ~DMA_CCR1_EN;
+  DMA1_Channel4->CPAR = (uint32_t)&(USART1->DR);
+  DMA1_Channel4->CCR = DMA_CCR1_DIR | DMA_CCR1_MINC; /* mem2per, no-circ, no-per-inc, mem-inc, psize=8, memsize=8,ch-prio=low, no-mem2mem */
+  /* configuring DMA for USART1-RX */
+  DMA1_Channel5->CCR &= ~DMA_CCR1_EN;
+  DMA1_Channel5->CPAR = (uint32_t)&(USART1->DR);
+  DMA1_Channel5->CCR = DMA_CCR1_MINC | DMA_CCR1_CIRC; /* per2mem, circ, no-per-inc, mem-inc, psize=8, memsize=8, ch-prio=low, no-mem2mem */
+  DMA1_Channel5->CMAR = (uint32_t)UART1_DMA_RX_BUFFER;
+  DMA1_Channel5->CNDTR = sizeof(UART1_DMA_RX_BUFFER);
+  DMA1_Channel5->CCR |= DMA_CCR1_EN;
+#endif
+  USART1->CR1 |= USART_CR1_UE; /* USART1 is enabled */
+}
+
 #if (CPU_TYPE == CPU_TYPE_STM32F1)
-#if F_SYSTEM == 48000
+#if (f_PLL_Hz == 48000000) && (f_APB1_Hz == 24000000)
 //#define USART2_BRR ((F_SYSTEM * 1000l * (8 * (2 - USART2_OVER8))) /  9600)
 //#define USART2_BRR (1000 << 4) /* testing clock generation of USART */
-#define USART2_BRR_9600   ((24000000/  9600) + 4) /* testing clock generation of USART */
-#define USART2_BRR_19200  ((24000000/ 19200) + 2) /* testing clock generation of USART */
-#define USART2_BRR_38400  ((24000000/ 38400) + 0) /* testing clock generation of USART */
-#define USART2_BRR_115200 ((24000000/115200) + 0) /* testing clock generation of USART */
+#define USART2_BRR_9600   ((f_APB1_Hz/  9600) + 4) /* testing clock generation of USART */
+#define USART2_BRR_19200  ((f_APB1_Hz/ 19200) + 2) /* testing clock generation of USART */
+#define USART2_BRR_38400  ((f_APB1_Hz/ 38400) + 0) /* testing clock generation of USART */
+#define USART2_BRR_115200 ((f_APB1_Hz/115200) + 0) /* testing clock generation of USART */
 #else
 #error Settings must be adapted for system configuration!
 #endif
@@ -52,7 +90,6 @@ void UART2_Init(uint32_t BaudRate)
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
   }
   USART2->CR1 = USART_CR1_TE | USART_CR1_RE; /* TX/RX are enabled */
-#if F_SYSTEM == 48000
   if (BaudRate == 9600)
   {
     USART2->BRR = USART2_BRR_9600;
@@ -73,9 +110,6 @@ void UART2_Init(uint32_t BaudRate)
   { /* wrong baud rate is specified -> error */
     USART2->BRR = USART2_BRR_9600;
   }
-#else
-#error "USART2_BRR must be adapted for system frequency!"
-#endif
   USART2->CR2 = 0; /* 1 stop bit */
 #if UART2_DMA == 0
   USART2->CR3 = 0x00; /* DMA is disabled (???) for the channel */
