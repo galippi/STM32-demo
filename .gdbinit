ac38005de2f915bc9284fcfe5618324f154bd514
Gabor Liptak <liptakgabor@freemail.hu>
2020-08-09 20:07:10 +0200

    Documentation is added

--- .gdbinit
+++ .gdbinit
@@ -4,4 +4,4 @@ monitor halt
 load bin/demo1.elf
 disconnect
 target remote localhost:3333
-monitor reset halt
+monitor reset init
---
+++ _ocd_stm32f103c8t6_program_bu2l.bat
@@ -0,0 +1,6 @@
+@set OCD_PATH=C:\KBApps\openocd-0.9.0
+::start %OCD_PATH%\bin-x64\openocd.exe -f scripts\board\st_nucleo_f103rb.cfg
+::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103rb.cfg
+::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103c8.cfg
+%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg -c "program ./bin/demo1.s19" -c "reset run" -c "exit"
+::%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg
