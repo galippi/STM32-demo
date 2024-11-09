640114a4c8e95af68eb3f558e886acb4fc06cf67
Gabor Liptak <liptakgabor@freemail.hu>
2024-11-09 21:10:26 +0100

    Code tidy: comments ar corrected

--- uart.c
+++ uart.c
@@ -18,13 +18,13 @@ void UART1_Init(uint32_t baudRate, uint8_t uartRemap)
 {
   if (uartRemap == 0)
   {
-    GPIO_PortInit_AFOut(GPIOA,  9); /* PA2 USART1_TX */
-    GPIO_PortInit_In(GPIOA, 10); /* PA3 USART1_RX */
+    GPIO_PortInit_AFOut(GPIOA,  9); /* PA9  USART1_TX */
+    GPIO_PortInit_In(GPIOA, 10);    /* PA10 USART1_RX */
     BitfieldSet(AFIO->MAPR, 2, 1, 0); /* AFIO_MAPR_USART1_REMAP */
   }else
   {
-    GPIO_PortInit_AFOut(GPIOB,  6); /* PA2 USART1_TX */
-    GPIO_PortInit_In(GPIOB,  7); /* PA3 USART1_RX */
+    GPIO_PortInit_AFOut(GPIOB,  6); /* PB6 USART1_TX */
+    GPIO_PortInit_In(GPIOB,  7);    /* PB7 USART1_RX */
     BitfieldSet(AFIO->MAPR, 2, 1, 1); /* AFIO_MAPR_USART1_REMAP */
   }
   RCC->APB2ENR |= RCC_APB2ENR_USART1EN;   /* enable the USART1 */
