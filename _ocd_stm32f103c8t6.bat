41a9fc14fa15a1e2ff539b7416020da269b1f6c0
Gabor Liptak <liptakgabor@freemail.hu>
2024-11-17 19:31:45 +0100

    OpenOCD scripts are adapted for STM32G030

--- _ocd_stm32f103c8t6.bat
+++ _ocd_stm32f103c8t6.bat
@@ -1,7 +1,8 @@
-@set OCD_PATH=C:\Programok\openocd-0.9.0
+@cls
+@set OCD_PATH=C:\Programok\OpenOCD-0.12.0-3
 @set START=start
 ::start %OCD_PATH%\bin-x64\openocd.exe -f scripts\board\st_nucleo_f103rb.cfg
 ::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103rb.cfg
 ::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103c8.cfg
-%START% %OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg
+%START% %OCD_PATH%\bin\openocd.exe -f %CD%\OpenOCD\st_nucleo_g0.cfg
 ::%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg
