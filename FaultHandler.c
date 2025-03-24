3bc89cefa9cd53b5adaa1b6a03860fe8888eb652
Gabor Liptak <liptakgabor@freemail.hu>
2025-03-24 20:19:20 +0100

    CAT error data is dumped for debugging

--- FaultHandler.c
+++ FaultHandler.c
@@ -1,7 +1,10 @@
-#include "FaultHandler.h"
+#include "util.h"
 #include "uart.h"
 #include "timer_conf.h"
 #include "timer.h"
+#include "u32_to_hexstring.h"
+
+#include "FaultHandler.h"
 
 t_CAT_ErrorCode CAT_Error_Code;
 uint32_t CAT_Error_SubCode;
@@ -25,8 +28,25 @@ static void CAT_SendData(const void *dataPtr, uint32_t len) {
     }
 }
 
+static void toHexString(void *dst, uint32_t val, unsigned len) {
+    char *ptr = (char*)dst + len - 1;
+    while(len > 0) {
+        uint8_t valU8 = val & 0xFF;
+        if (len & 0x01)
+            valU8 = valU8 >> 4;
+        val = val >> 8;
+        *ptr = toHexDigit(valU8);
+        ptr--;
+        len--;
+    }
+}
+
 static void CAT_SendDebugData(void) {
     static char debugData[] = "CATxxxxyyyy\r";
+    //toHexString(debugData + 3, CAT_Error_Code, 4);
+    U32_to_HexString(debugData + 3, 4, CAT_Error_Code, '0');
+    //toHexString(debugData + 7, CAT_Error_SubCode, 4);
+    U32_to_HexString(debugData + 7, 4, CAT_Error_SubCode, '0');
     CAT_SendData(debugData, sizeof(debugData) - 1);
 }
 
@@ -37,10 +57,9 @@ static void CAT_SendDebugData(void) {
 static void CAT_WaitMs(uint32_t ms) {
     uint16_t t_start = TIM3_Cnt_Get();
     while(ms != 0) {
-        //TIM3_FREQ
         while (1) {
             uint16_t t = TIM3_Cnt_Get();
-            if ((t - t_start) > TIM3_1ms) {
+            if (((t - t_start) & 0xFFFF) >= TIM3_1ms) {
                 t_start += TIM3_1ms;
                 break;
             }
@@ -62,7 +81,8 @@ void CAT_Error(t_CAT_ErrorCode Code, uint32_t SubCode)
   CAT_SystemInit();
   while(1) { /* endless loop */
       CAT_SendDebugData();
-      CAT_WaitMs(1000);
+      systemSleepMs(1000);
+      //CAT_WaitMs(1000);
   }
 }
 
