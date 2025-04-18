cb1be4566604e05844c6d7efee8f7e712093a84e
Gabor Liptak <liptakgabor@freemail.hu>
2025-04-18 19:50:59 +0200

    ST-link utility starter script

---
+++ _st_link.bat
@@ -0,0 +1,3 @@
+:: If the OpenOcd cannot connect, then pull reset to low, connect ST-link utility,
+:: release reset and if the connection is succesful, then erase flash.
+"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\stm32 st-link utility.exe"
