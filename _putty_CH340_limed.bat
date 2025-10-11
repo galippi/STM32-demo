5522ef0e2ebb56554ff657c92b61636d2541ed2a
Gabor Liptak <liptakgabor@freemail.hu>
2025-10-11 20:23:12 +0200

    Debug info: UART interface cabling info is added

--- _putty_CH340_limed.bat
+++ _putty_CH340_limed.bat
@@ -1,2 +1,13 @@
-start C:\Programok\putty.exe -load ch340
-::start C:\Programok\putty.exe
+FTDI:
+GND - GND
+RX  - B7
+TX  - B6
+VCC - 3V3
+
+CH340:
+:: black  - GND
+:: orange - B7
+:: braun  - B6
+
+::start C:\Programok\putty.exe -load ch340
+start C:\Programok\putty.exe
