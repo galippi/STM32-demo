504110a4e62d2bb3f58de44fa071b1fe6a33b770
Gabor Liptak <liptakgabor@freemail.hu>
2025-10-12 10:16:40 +0200

    Fix: CAN filter setting is added

--- can_hal.h
+++ can_hal.h
@@ -3,6 +3,8 @@
 static inline void can1_init(uint32_t baud)
 {
   CAN_STM32_init(baud);
+  CAN_STM32_setFilter(CAN1,  0, CAN_Filter_MASK_32bit, 0x00000000, 0x00000000); // all messages are accepted
+  //CAN_STM32_setFilter(CAN1,  1, CAN_Filter_MASK_32bit, 0x000001F0, 0x800007FF);
 }
 
 #define can1_deinit() CAN_STM32_deinit()
