ae65384da38ac6215a6a9890437c794a77637194
Gabor Liptak <liptakgabor@freemail.hu>
2025-10-18 22:20:29 +0200

    New line character is configurable Error report for too long CAN message is added ID char of received CAN message is configurable

--- LwSlcan_conf.h
+++ LwSlcan_conf.h
@@ -1,3 +1,6 @@
+#ifndef _LWSLCAN_CONF_H_
+#define _LWSLCAN_CONF_H_
+
 typedef struct {
     uint8_t uart1_rxCtr;
     uint8_t CAN_txCtr;
@@ -6,8 +9,13 @@ typedef struct {
 
 extern t_LwSlcanDbg dbgLwSlcan;
 
+#define SLCAN_NEWLINE_STR "\n"
+#define SLCAN_NEWLINE_PUT(str_ptr) (*(str_ptr) = '\n', (str_ptr)++, (str_ptr))
+
 #if 1
 #define DBG_INC(var) do {dbgLwSlcan.var++;}while(0)
 #else
 #define DBG_INC(var) do {}while(0) /* do nothing */
 #endif
+
+#endif /* _LWSLCAN_CONF_H_ */
