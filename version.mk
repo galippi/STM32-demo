bad2b40bf0db694844aa7a55fff52e1c1b366e91
Gabor Liptak <liptakgabor@freemail.hu>
2025-10-22 19:52:46 +0200

    git-tag determination is corrected

--- version.mk
+++ version.mk
@@ -9,10 +9,26 @@ VERSION_OLD:=(none)
 endif
 
 GIT_COMMIT_ID := $(shell git rev-parse --short --verify HEAD 2>/dev/null)
+MODIFIED := $(shell git status --short 2> /dev/null)
+LAST_TAG := $(shell git describe --tags --exact-match 2> /dev/null)
 
 ifeq ($(GIT_COMMIT_ID),)
 GIT_COMMIT_ID := (git error)
+else
+  ifeq ($(MODIFIED),)
+    # not modified version
+    ifeq ($(LAST_TAG),)
+      # no tag - keep GIT_COMMIT_ID
+    else
+      GIT_COMMIT_ID := $(LAST_TAG)
+    endif
+  else
+    # modified content
+    GIT_COMMIT_ID := $(GIT_COMMIT_ID)-modified
+  endif
 endif
+
+VERSION_FILE = $(wildcard version.h)
 
 VERSION_NEW := \#define GIT_COMMIT_ID "$(GIT_COMMIT_ID)"
 
@@ -23,11 +39,19 @@ VERSIONFLAGS += -DFILE_SYNC_VERSION="$(VERSION_OLD)" -DFILE_SYNC_GIT="$(GIT_COMM
 #CPPFLAGS += $(VERSIONFLAGS)
 
 ifneq ($(VERSION_OLD),$(VERSION_NEW))
-.PHONY : version.h
+
+.PHONY: version.h
+
 #version.h: version_h_debug
 version.h:
 	@echo Generating $@
 	echo '$(VERSION_NEW)' >$@
+
+else
+
+version.h:
+	@echo Generating $@ is skipped
+
 endif
 
 .PHONY : version_h_debug
@@ -35,3 +59,4 @@ version_h_debug:
 	@echo Generating $@
 	@echo "VERSION_OLD = $(VERSION_OLD)"
 	@echo "VERSION_NEW = $(VERSION_NEW)"
+	@echo VERSION_FILE=$(VERSION_FILE)
