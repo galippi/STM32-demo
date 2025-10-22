971f43d99eabb3abf3495ab367f02cd8bf3e0b12
Gabor Liptak <liptakgabor@freemail.hu>
2025-10-22 19:51:30 +0200

    git script is simplified

--- _git_cmd.bat
+++ _git_cmd.bat
@@ -1,4 +1,7 @@
-set path=C:\Programs\Git\bin\;%PATH%
-set HOME=C:\Data\Liptak
+::set path=C:\Programs\Git\bin\;%PATH%
+::set HOME=C:\Data\Liptak
 ::set HOME=
+
+@call _git_config.bat
+
 start "%CD%" cmd
