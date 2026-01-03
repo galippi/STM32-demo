d3a709977ed651bffe9b992ad184a06584ecea27
Gabor Liptak <liptakgabor@freemail.hu>
2026-01-03 10:44:44 +0100

    STM - ST - link starter scripts are added

---
+++ _st_ST-Link_update.bat
@@ -0,0 +1,1 @@
+java -jar "D:\Program Files (x86)\STMicroelectronics\STM32Cube\STM32CubeProgrammer\Drivers\FirmwareUpgrade\stlinkupgrade.jar"
---
+++ _st_STM32CubeProgrammer.bat
@@ -0,0 +1,1 @@
+"D:\Program Files (x86)\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\stm32cubeprogrammer.exe"
