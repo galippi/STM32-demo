b2002385a3db03aa8e53170231ffad640ddb2395
Gabor Liptak <liptakgabor@freemail.hu>
2026-01-03 10:36:50 +0100

    OCD version is changed from 0.9.0 to 0.12.0-3 to support STM32G0 Start putty - OpenOcd, if external start is executed

--- _ocd_stm32g030.bat
+++ _ocd_stm32g030.bat
@@ -1,8 +1,26 @@
 @cls
 @set OCD_PATH=C:\Programok\OpenOCD-0.12.0-3
+::@set OCD_PATH=C:\Programok\openocd-0.9.0
 @set START=start
 ::start %OCD_PATH%\bin-x64\openocd.exe -f scripts\board\st_nucleo_f103rb.cfg
 ::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103rb.cfg
 ::start %OCD_PATH%\bin-x64\openocd.exe -f %OCD_PATH%\scripts\board\st_nucleo_f103c8.cfg
 %START% %OCD_PATH%\bin\openocd.exe -f %CD%\OpenOCD\st_nucleo_g0.cfg
-::%OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg
+::%START% %OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_g0.cfg
+::%START% %OCD_PATH%\bin\openocd.exe -f %CD%\OpenOCD\st_nucleo_g0.cfg -d3
+::%START% %OCD_PATH%\bin\openocd.exe -f %CD%\OpenOCD\st_nucleo_g0.cfg  -c "init; reset halt"
+::%START% %OCD_PATH%\bin-x64\openocd.exe -f %CD%\OpenOCD\st_nucleo_f103c8.cfg
+
+@if "%START%"=="" goto wait_to_user
+
+sleep 2s
+::start C:\Programok\putty.exe -load openocd
+call _putty_OpenOCD_telnet_limed.bat
+
+goto end
+
+:wait_to_user
+pause
+goto end
+
+:end
