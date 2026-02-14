8f3547c404e2d9375a42495fa8b0383298c7a758
Gabor Liptak <liptakgabor@freemail.hu>
2026-02-14 14:07:54 +0100

    Path of IotTerminal is modified to branched version

--- _t_iot.bat
+++ _t_iot.bat
@@ -3,4 +3,4 @@
 @set PATH=%PATH%;D:\Gabor\Java\IotTerminal\IotTerminal\lib\RXTXcomm
 @set JAVA=C:\Program Files (x86)\Java\jdk1.7.0_11
 ::@java -jar D:\Gabor\Java\IotTerminal\IotTerminal\bin\iotterminal.jar
-%START% "%JAVA%\bin\java" -jar D:\Gabor\Java\IotTerminal\IotTerminal\bin\iotterminal.jar
+%START% "%JAVA%\bin\java" -jar D:\Gabor\Java\IotTerminal\IotTerminal.battery\bin\iotterminal.jar
